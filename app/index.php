ce06dfce71145f5d83d2efdb30b4ed6e35bc67d1
RubiDickens <15586141.clot@fje.edu>
2024-01-17 19:47:51 +0000

    Commit 1.3 de l'aplicació ipcalc

--- app/index.php
+++ app/index.php
@@ -9,7 +9,7 @@
 	<body>
 		<p><b>CALCUALDORA IPv4</b></p>
 		<a href="ip.html">Aplicació de càlcul de subxarxes</a>
-		<p>Autor: aneas<p>
+		<p>Autor: asix2 - versio 1.4<p>
 <?php
  echo date("l jS \of F Y h:i:s A") . "<br>";
 ?>
